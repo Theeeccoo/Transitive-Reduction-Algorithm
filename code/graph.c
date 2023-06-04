b548820aef615c754f638e567ce0a3a34b5d0985
Theeeccoo <thiagodecampos1@gmail.com>
2023-06-03 21:38:58 -0300

    Fixing and implementing Graph essentials operations

--- code/graph.c
+++ code/graph.c
@@ -1,57 +1,248 @@
 #include "graph.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * @brief Initializes vertices and edges
  * 
- * @param vertices_num Number of desired vertices
- * @param edges_num Number of desired vertices
+ * @param number_of_vertices Number of desired vertices
+ * @param number_of_edges Number of desired vertices
  *
  * @details Allocates memory space based on sizes given as parameters
  *
  * @returns Reference to newly create Graph
  */
 
-Graph* graph_initializer(int vertices_num, int edges_num){
-	if (vertices_num >= MAX_SIZE || edges_num >= MAX_SIZE) return NULL;
+Graph* graph_initializer(int number_of_vertices, int number_of_edges){
+	if ( number_of_vertices >= MAX_AMOUNT || number_of_edges >= MAX_AMOUNT ) {
+		printf("ERROR: You have reached the maximum number of vertices and edges in your Graph (%d)\n", MAX_AMOUNT);
+		return NULL;
+	}
 
-	Graph *g = (Graph*) malloc(sizeof(Graph));
-	g->vertices = (STRING) malloc(sizeof(char) * vertices_num + 1);
-	g->edges = (STRING) malloc(sizeof(char) * edges_num + 1);
+	Graph *g = (Graph*) malloc( sizeof(Graph) );
 
+	g->vertices = (STRING*) malloc( sizeof(STRING) * number_of_vertices + 1 );
+	g->vertices_allocated = number_of_vertices;
+
+	g->edges = (STRING**) malloc( sizeof(STRING*) * number_of_edges + 1 );
+	g->edges_allocated = number_of_edges;
+
+	g->edges_neighbours = (int*) calloc( number_of_vertices, sizeof(int) );
 
 	return g;
 }
 
-void graph_add_vertice(Graph* graph, const char* vertice){
-	if (graph->vertices_num >= SISE_STR) {
-        printf("Error: the limit has been reached.\n");
-        return;
-    }
-	//usar strcpy para adiconar no lugar certo
-	graph->vertices_num++;
+/*
+ * @brief Insert given vertice into graph
+ *
+ * @param graph Graph to have the new vertice
+ * @param vertice Vertice to be inserted
+ *
+ * @details Receives a vertice, verify if there is free space to add it and if it is unique in our graph.
+ *
+ * @retuns If allocation was successfully, returns where vertice was inserted
+ *         OTHERWISE, returns -1 (ERROR) 
+ */
+
+int graph_add_vertice(Graph* graph, STRING vertice){
+	int	controller = -1,
+		aux	   =  0,
+		position   =  0;
+
+	if ( graph->vertices_amount >= graph->vertices_allocated ) {
+		printf("ERROR: the limit (%d) has been reached.\n", graph->vertices_allocated);
+		return controller;
+	}
+
+	if ( ( aux = graph_vertice_finder(graph, vertice) ) != -1 ) {
+		printf("ERROR: vertice (%s) was already found in Graph at position: %d\n", vertice, aux);
+		return controller;
+	}
+	
+	position = graph->vertices_amount;
+
+	// Alocatting memory on demand
+	graph->vertices[position] = (STRING) malloc( sizeof(char) * STR_SIZE + 1);
+	strcpy( graph->vertices[position], vertice );
+	graph->vertices_amount++;
+
+	graph->edges[position] = (STRING*) malloc( sizeof(STRING) * graph->vertices_allocated );
+	
+	return position;
+}
+
+/*
+ * @brief Insert edges on given vertices
+ *
+ * @param graph Graph to have the new edge
+ * @param first_vertice Vertice to have a new edge added
+ * @param second_vertice Vertice to have a new edge added
+ * @param flag Flag to control insertion
+ *
+ * @details Receives two vertices in order to create a new edge between them. 
+ * FLAG variable defines if the insertion will be directed or non-directed.
+ * Using auxiliary array (edges_neighbours) to control the amount of neighbours that given vertice has, 
+ * 	useful to know which is the next line so we can add a new neighbour.
+ *
+ * The position that given vertice has on Vertices Array is equal to the position of its neighbours on Edges Array
+ * ( So, basically Edges Array = Actual neighbours of given vertice ) 
+ *
+ * @returns IF vertices not found in graph OR vertice is already in its Edges Array, return -1 (ERROR)
+ * 	    OTHERWISE, return 0 (INSERTION OK)
+           
+ */
+
+int graph_add_edge(Graph* graph, const char* first_vertice, const char* second_vertice, int flag){
+	int	controller = -1,
+		pos_first  =  0,
+		pos_second =  0;
+
+	// Vertice not found in graph
+	if ( (( pos_first = graph_vertice_finder(graph, first_vertice)) == -1 ) || (( pos_second = graph_vertice_finder(graph, second_vertice)) == -1 ) )  {
+		printf("ERROR: Atleast one of your vertices (%s - %s) was not found in your graph.\n", first_vertice, second_vertice);
+		return controller;
+	}
+
+	// Non-direct graph 
+	if ( flag == 0 ) {
+		int	first_neighbours_amount = graph->edges_neighbours[pos_first], 
+			second_neighbours_amount = graph->edges_neighbours[pos_second];
+
+		// Atleast one of the vertices is already inserted
+		if( graph_edge_finder(graph, pos_first, second_vertice) != -1 || graph_edge_finder(graph, pos_second, first_vertice) != -1 ){
+			printf("ERROR: One of your vertices (%s %s) was already inserted in List of Neighbours\n", first_vertice, second_vertice);
+			return controller;
+		}
+
+		// Alocatting lines on demand
+		graph->edges[pos_first][first_neighbours_amount]   = (STRING) malloc( sizeof(char) * STR_SIZE + 1 );
+		graph->edges[pos_second][second_neighbours_amount] = (STRING) malloc( sizeof(char) * STR_SIZE + 1 );
+
+		// Inserting values on lines
+		strcpy( graph->edges[pos_first][first_neighbours_amount], second_vertice );
+		strcpy( graph->edges[pos_second][second_neighbours_amount], first_vertice );
+
+		// Incrementing number of neighbours that given vertice has
+		graph->edges_neighbours[pos_first] += 1;
+		graph->edges_neighbours[pos_second] += 1;
+
+	} else {
+		int	first_neighbours_amount = graph->edges_neighbours[pos_first];
+		
+		// Other vertice is already inserted
+		if ( graph_edge_finder(graph, pos_first, second_vertice) != -1 ) {
+			printf("ERROR: Your destination vertice (%s) is already inserted in List of Neighbours of source (%s)\n", second_vertice, first_vertice);
+			return controller;
+		}
+		// Alocatting line on demand
+		graph->edges[pos_first][first_neighbours_amount] = (STRING) malloc( sizeof(char) * STR_SIZE + 1 );
+
+		// Inserting value on line
+		strcpy( graph->edges[pos_first][first_neighbours_amount], second_vertice );
+		
+		// Incrementing number of neighbours that given vertice has
+		graph->edges_neighbours[pos_first] += 1;
+	}
+
+	return 0;
+}
+
+
+/*
+ * @brief Returns, if existing, the position of given vertice
+ *
+ * @param graph Graph to be iterated
+ * @param vertice Vertice to be found
+ *
+ * @details Iterate through graph vertices list until END OF MATRIX or vertice found.
+ *
+ * @returns IF END OF MATRIX, return -1 (Vertice not found)
+ * 	    OTHERWISE, return index of give vertice
+ */
+
+int graph_vertice_finder(Graph* graph, const char* vertice) {
+	int	position = -1,
+		i 	 =  0;
+
+
+	for ( ; i < graph->vertices_amount ; i++ ) {
+		if( strcmp(graph->vertices[i], vertice) == 0 ) {
+			position = i;
+			return position;
+		}
+	}
+
+	return position;
+}
+
+/*
+ * @brief Returns, if existing, the position of given vertice's neighbours in Edge list
+ * 
+ * @param graph Graph to be iterated
+ * @param vertice_position Position of the vertice's Neighbours list
+ * @param to_be_found Vertice to be found as neighbour
+ *
+ * @details Iterate through vertice_position Edge list until END OF LIST or to_be_found found.
+ * 
+ * @returns IF END OF LIST, return -1 (Neighbour not found)
+ * 	    OTHERWISE, return index of given vertice
+ * 
+ */
+
+int graph_edge_finder(Graph* graph, int vertice_position, const char* to_be_found){
+	int	position    = -1,
+		i 	    =  0,
+		neighbours_amount = graph->edges_neighbours[vertice_position];
+
+
+	for( ; i < neighbours_amount; i++ ){
+		if( strcmp(graph->edges[vertice_position][i], to_be_found ) == 0 ) {
+			position = i;
+			return position;
+		}
+	}
+
+	return position;
+}
+
+
+void graph_print_vertices(Graph* graph){
+	int	i = 0;
+
+	printf("Vertices of your graph: \n\t");
+	for( ; i < graph->vertices_amount; i++){
+		printf("%s-\t", graph->vertices[i]); 
+	}
+	printf("\n");
+}
+
+void graph_print_edges(Graph* graph){
+	int	i = 0,
+		j = 0; 
+
+	printf("Edges of your graph: \n");	
+
+	for( ; i < graph->vertices_amount ; i++ ){
+		
+		printf("%s's neighbours: \n\t", graph->vertices[i]);
+		int	neighbours = graph->edges_neighbours[i];
+		if ( neighbours == 0 ) { printf("EMPTY\n"); }
+		else {
+			for( j = 0; j < neighbours; j++ ){
+				printf("%s-\t", graph->edges[i][j]);
+			}
+		}
+		printf("\n");
+	}
 
 }
-void graph_add_edge(Graph* graph, const char* source, const char* destination){
-	int source_index = -1;
-    int destination_index = -1;
 
-	//find vertice
-
-	//a validation
-	if (source_index == -1 || destination_index == -1) {
-        printf("Error: vertice not found.\n");
-        return;
-    }
-
-	//conecte the edge
-
-}
 
 
 void graph_destroy(Graph* graph) {
 	//ver se pode usar a função free 
-    free(graph->vertices);
-    free(graph->edges);
-    free(graph);
+	free(graph->vertices);
+	free(graph->edges);
+	free(graph);
 }
--- code/teste.c
+++ code/teste.c
@@ -9,12 +9,21 @@ int main(void){
 
 	printf("Desired ammount of vertices: ");
 	scanf("%d", &vertices);
-	fflush(stdin);
 	printf("\nDesired ammount of edges: ");
 	scanf("%d", &edges);
-	fflush(stdin);
 
-	printf("%d, %d", vertices, edges);
+	printf("%d, %d\n", vertices, edges);
 	Graph *g = graph_initializer(vertices, edges);
+
+
+	graph_add_vertice(g, "a");
+	graph_add_vertice(g, "b");
+	graph_add_vertice(g, "c");
+	graph_add_edge(g, "a", "b", 0);
+	graph_add_edge(g, "a", "c", 1);
+	
+	graph_print_vertices(g);
+	graph_print_edges(g);
+
 	return 0;	
 }
